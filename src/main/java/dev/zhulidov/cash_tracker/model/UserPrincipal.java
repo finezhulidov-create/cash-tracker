e1776de7a2bcdcc00e9a05cd56640156ac9d961c
ZhulidovAl <fine.zhulidov@gmail.com>
2026-07-01 12:42:42 +0500

    fix: create UserPrincipal.java

---
+++ src/main/java/dev/zhulidov/cash_tracker/model/UserPrincipal.java
@@ -0,0 +1,40 @@
+package dev.zhulidov.cash_tracker.model;
+
+import lombok.*;
+import org.springframework.context.support.BeanDefinitionDsl;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class UserPrincipal implements UserDetails {
+    private Long id;
+    private String email;
+    private String userName;
+    private String password;
+    public static UserPrincipal from(User user){
+        return new UserPrincipal(user.getId(),user.getEmail(),user.getName(),user.getPassword());
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+    /**
+     * Возвращает email пользователя, используемый в качестве имени пользователя для аутентификации.
+     *
+     * @return email пользователя (уникальный идентификатор при входе)
+     */
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
