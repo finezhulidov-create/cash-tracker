dd8067e2204fe12a22dc8540710cbd7cab17959d
ZhulidovAl <fine.zhulidov@gmail.com>
2026-07-01 12:42:20 +0500

    fix: Обернул юзера в юзер принципал, чтобы не передавать сущность из бд на прямую

--- src/main/java/dev/zhulidov/cash_tracker/service/CustomUserDetailsService.java
+++ src/main/java/dev/zhulidov/cash_tracker/service/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package dev.zhulidov.cash_tracker.service;
 
 import dev.zhulidov.cash_tracker.exception.ResourceNotFoundException;
+import dev.zhulidov.cash_tracker.model.UserPrincipal;
 import dev.zhulidov.cash_tracker.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,7 +15,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository repository;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return repository.findByEmail(email)
-                .orElseThrow(()-> new ResourceNotFoundException("User not found by this email: " + email));
+        return UserPrincipal.from(repository.findByEmail(email)
+                .orElseThrow(()-> new ResourceNotFoundException("User not found by this email: " + email)));
     }
 }
