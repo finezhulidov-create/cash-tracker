f465ac0b1dfa31f19eae93442c7b613e333d1faf
ZhulidovAl <fine.zhulidov@gmail.com>
2026-07-04 13:18:36 +0500

    epik1: add Transaction entity

--- cash-tracker-transactions/src/main/java/dev/zhulidov/cash_tracker/transactions/model/Transaction.java
+++ cash-tracker-transactions/src/main/java/dev/zhulidov/cash_tracker/transactions/model/Transaction.java
@@ -1,4 +1,33 @@
 package dev.zhulidov.cash_tracker.transactions.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "transaction")
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "date", nullable = false)
+    private LocalDateTime dateTime;
+    @OneToMany(mappedBy = "transaction")
+    private List<TransactionSplit> splits;
+
 }
