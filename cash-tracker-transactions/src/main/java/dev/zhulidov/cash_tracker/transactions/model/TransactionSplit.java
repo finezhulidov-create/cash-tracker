e6ac0ff3538c0e4ff901fab4cdc44e9faa6ca1db
ZhulidovAl <fine.zhulidov@gmail.com>
2026-07-04 13:05:49 +0500

    add: id field

--- cash-tracker-transactions/src/main/java/dev/zhulidov/cash_tracker/transactions/model/TransactionSplit.java
+++ cash-tracker-transactions/src/main/java/dev/zhulidov/cash_tracker/transactions/model/TransactionSplit.java
@@ -1,4 +1,10 @@
 package dev.zhulidov.cash_tracker.transactions.model;
 
 public class TransactionSplit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+
 }
